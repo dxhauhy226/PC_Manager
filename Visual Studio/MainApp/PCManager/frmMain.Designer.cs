d08a8928154b12ffb46375bb494a3b8dec731b45
Đinh Xuân Hậu <xuanhau2k@gmail.com>
2023-02-14 23:57:50 +0700

    Add UI

---
+++ Visual Studio/MainApp/PCManager/frmMain.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace PCManager
+{
+	partial class frmMain
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.mnuFile = new System.Windows.Forms.ToolStripMenuItem();
+			this.mnuExit = new System.Windows.Forms.ToolStripMenuItem();
+			this.mnuCategory = new System.Windows.Forms.ToolStripMenuItem();
+			this.mnuManufacturer = new System.Windows.Forms.ToolStripMenuItem();
+			this.mnuComputer = new System.Windows.Forms.ToolStripMenuItem();
+			this.mnuStaff = new System.Windows.Forms.ToolStripMenuItem();
+			this.mnuCustomer = new System.Windows.Forms.ToolStripMenuItem();
+			this.billToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.mnuBill = new System.Windows.Forms.ToolStripMenuItem();
+			this.mnuSearch = new System.Windows.Forms.ToolStripMenuItem();
+			this.mnuSearchBill = new System.Windows.Forms.ToolStripMenuItem();
+			this.mnuSearchComputer = new System.Windows.Forms.ToolStripMenuItem();
+			this.mnuSearchCustomer = new System.Windows.Forms.ToolStripMenuItem();
+			this.mnuHelp = new System.Windows.Forms.ToolStripMenuItem();
+			this.mnuAbout = new System.Windows.Forms.ToolStripMenuItem();
+			this.mnuGetStarted = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFile,
+            this.mnuCategory,
+            this.billToolStripMenuItem,
+            this.mnuSearch,
+            this.mnuHelp});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// mnuFile
+			// 
+			this.mnuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuExit});
+			this.mnuFile.Name = "mnuFile";
+			this.mnuFile.Size = new System.Drawing.Size(37, 20);
+			this.mnuFile.Text = "File";
+			// 
+			// mnuExit
+			// 
+			this.mnuExit.Name = "mnuExit";
+			this.mnuExit.Size = new System.Drawing.Size(93, 22);
+			this.mnuExit.Text = "Exit";
+			// 
+			// mnuCategory
+			// 
+			this.mnuCategory.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuManufacturer,
+            this.mnuComputer,
+            this.mnuStaff,
+            this.mnuCustomer});
+			this.mnuCategory.Name = "mnuCategory";
+			this.mnuCategory.Size = new System.Drawing.Size(67, 20);
+			this.mnuCategory.Text = "Category";
+			// 
+			// mnuManufacturer
+			// 
+			this.mnuManufacturer.Name = "mnuManufacturer";
+			this.mnuManufacturer.Size = new System.Drawing.Size(146, 22);
+			this.mnuManufacturer.Text = "Manufacturer";
+			// 
+			// mnuComputer
+			// 
+			this.mnuComputer.Name = "mnuComputer";
+			this.mnuComputer.Size = new System.Drawing.Size(146, 22);
+			this.mnuComputer.Text = "Computer";
+			// 
+			// mnuStaff
+			// 
+			this.mnuStaff.Name = "mnuStaff";
+			this.mnuStaff.Size = new System.Drawing.Size(146, 22);
+			this.mnuStaff.Text = "Staff";
+			// 
+			// mnuCustomer
+			// 
+			this.mnuCustomer.Name = "mnuCustomer";
+			this.mnuCustomer.Size = new System.Drawing.Size(146, 22);
+			this.mnuCustomer.Text = "Customer";
+			// 
+			// billToolStripMenuItem
+			// 
+			this.billToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuBill});
+			this.billToolStripMenuItem.Name = "billToolStripMenuItem";
+			this.billToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+			this.billToolStripMenuItem.Text = "Bill";
+			// 
+			// mnuBill
+			// 
+			this.mnuBill.Name = "mnuBill";
+			this.mnuBill.Size = new System.Drawing.Size(90, 22);
+			this.mnuBill.Text = "Bill";
+			// 
+			// mnuSearch
+			// 
+			this.mnuSearch.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuSearchBill,
+            this.mnuSearchComputer,
+            this.mnuSearchCustomer});
+			this.mnuSearch.Name = "mnuSearch";
+			this.mnuSearch.Size = new System.Drawing.Size(54, 20);
+			this.mnuSearch.Text = "Search";
+			// 
+			// mnuSearchBill
+			// 
+			this.mnuSearchBill.Name = "mnuSearchBill";
+			this.mnuSearchBill.Size = new System.Drawing.Size(128, 22);
+			this.mnuSearchBill.Text = "Bill";
+			// 
+			// mnuSearchComputer
+			// 
+			this.mnuSearchComputer.Name = "mnuSearchComputer";
+			this.mnuSearchComputer.Size = new System.Drawing.Size(128, 22);
+			this.mnuSearchComputer.Text = "Computer";
+			// 
+			// mnuSearchCustomer
+			// 
+			this.mnuSearchCustomer.Name = "mnuSearchCustomer";
+			this.mnuSearchCustomer.Size = new System.Drawing.Size(128, 22);
+			this.mnuSearchCustomer.Text = "Customer";
+			// 
+			// mnuHelp
+			// 
+			this.mnuHelp.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuAbout,
+            this.mnuGetStarted});
+			this.mnuHelp.Name = "mnuHelp";
+			this.mnuHelp.Size = new System.Drawing.Size(44, 20);
+			this.mnuHelp.Text = "Help";
+			// 
+			// mnuAbout
+			// 
+			this.mnuAbout.Name = "mnuAbout";
+			this.mnuAbout.Size = new System.Drawing.Size(132, 22);
+			this.mnuAbout.Text = "About";
+			// 
+			// mnuGetStarted
+			// 
+			this.mnuGetStarted.Name = "mnuGetStarted";
+			this.mnuGetStarted.Size = new System.Drawing.Size(132, 22);
+			this.mnuGetStarted.Text = "Get Started";
+			// 
+			// frmMain
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "frmMain";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "PC Manager";
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem mnuFile;
+		private System.Windows.Forms.ToolStripMenuItem mnuExit;
+		private System.Windows.Forms.ToolStripMenuItem mnuCategory;
+		private System.Windows.Forms.ToolStripMenuItem mnuManufacturer;
+		private System.Windows.Forms.ToolStripMenuItem mnuComputer;
+		private System.Windows.Forms.ToolStripMenuItem mnuStaff;
+		private System.Windows.Forms.ToolStripMenuItem mnuCustomer;
+		private System.Windows.Forms.ToolStripMenuItem billToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem mnuBill;
+		private System.Windows.Forms.ToolStripMenuItem mnuSearch;
+		private System.Windows.Forms.ToolStripMenuItem mnuSearchBill;
+		private System.Windows.Forms.ToolStripMenuItem mnuSearchComputer;
+		private System.Windows.Forms.ToolStripMenuItem mnuSearchCustomer;
+		private System.Windows.Forms.ToolStripMenuItem mnuHelp;
+		private System.Windows.Forms.ToolStripMenuItem mnuAbout;
+		private System.Windows.Forms.ToolStripMenuItem mnuGetStarted;
+	}
+}
+
---
+++ Visual Studio/MainApp/PCManager/frmManufacturer.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace PCManager
+{
+	partial class frmManufacturer
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.txtManufacturerID = new System.Windows.Forms.TextBox();
+			this.txtManufacturerName = new System.Windows.Forms.TextBox();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.btnAdd = new System.Windows.Forms.Button();
+			this.btnDelete = new System.Windows.Forms.Button();
+			this.btnEdit = new System.Windows.Forms.Button();
+			this.btnSave = new System.Windows.Forms.Button();
+			this.btnSkip = new System.Windows.Forms.Button();
+			this.btnExit = new System.Windows.Forms.Button();
+			this.dgvManufacturer = new System.Windows.Forms.DataGridView();
+			this.Manufacturer_ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Manufacturer_Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.groupBox1.SuspendLayout();
+			this.groupBox2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dgvManufacturer)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.txtManufacturerName);
+			this.groupBox1.Controls.Add(this.txtManufacturerID);
+			this.groupBox1.Controls.Add(this.label2);
+			this.groupBox1.Controls.Add(this.label1);
+			this.groupBox1.Location = new System.Drawing.Point(62, 12);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(361, 103);
+			this.groupBox1.TabIndex = 0;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Manufacturer";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(17, 25);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(84, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Manufacturer ID";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(17, 56);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(101, 13);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Manufacturer Name";
+			// 
+			// txtManufacturerID
+			// 
+			this.txtManufacturerID.Location = new System.Drawing.Point(142, 22);
+			this.txtManufacturerID.Name = "txtManufacturerID";
+			this.txtManufacturerID.Size = new System.Drawing.Size(200, 20);
+			this.txtManufacturerID.TabIndex = 2;
+			// 
+			// txtManufacturerName
+			// 
+			this.txtManufacturerName.Location = new System.Drawing.Point(142, 53);
+			this.txtManufacturerName.Name = "txtManufacturerName";
+			this.txtManufacturerName.Size = new System.Drawing.Size(200, 20);
+			this.txtManufacturerName.TabIndex = 3;
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.dgvManufacturer);
+			this.groupBox2.Location = new System.Drawing.Point(12, 121);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(480, 153);
+			this.groupBox2.TabIndex = 1;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "Data";
+			// 
+			// btnAdd
+			// 
+			this.btnAdd.Location = new System.Drawing.Point(12, 280);
+			this.btnAdd.Name = "btnAdd";
+			this.btnAdd.Size = new System.Drawing.Size(75, 23);
+			this.btnAdd.TabIndex = 2;
+			this.btnAdd.Text = "Add";
+			this.btnAdd.UseVisualStyleBackColor = true;
+			// 
+			// btnDelete
+			// 
+			this.btnDelete.Location = new System.Drawing.Point(93, 280);
+			this.btnDelete.Name = "btnDelete";
+			this.btnDelete.Size = new System.Drawing.Size(75, 23);
+			this.btnDelete.TabIndex = 3;
+			this.btnDelete.Text = "Delete";
+			this.btnDelete.UseVisualStyleBackColor = true;
+			// 
+			// btnEdit
+			// 
+			this.btnEdit.Location = new System.Drawing.Point(174, 280);
+			this.btnEdit.Name = "btnEdit";
+			this.btnEdit.Size = new System.Drawing.Size(75, 23);
+			this.btnEdit.TabIndex = 4;
+			this.btnEdit.Text = "Edit";
+			this.btnEdit.UseVisualStyleBackColor = true;
+			// 
+			// btnSave
+			// 
+			this.btnSave.Location = new System.Drawing.Point(255, 280);
+			this.btnSave.Name = "btnSave";
+			this.btnSave.Size = new System.Drawing.Size(75, 23);
+			this.btnSave.TabIndex = 5;
+			this.btnSave.Text = "Save";
+			this.btnSave.UseVisualStyleBackColor = true;
+			// 
+			// btnSkip
+			// 
+			this.btnSkip.Location = new System.Drawing.Point(336, 280);
+			this.btnSkip.Name = "btnSkip";
+			this.btnSkip.Size = new System.Drawing.Size(75, 23);
+			this.btnSkip.TabIndex = 6;
+			this.btnSkip.Text = "Skip";
+			this.btnSkip.UseVisualStyleBackColor = true;
+			// 
+			// btnExit
+			// 
+			this.btnExit.Location = new System.Drawing.Point(417, 280);
+			this.btnExit.Name = "btnExit";
+			this.btnExit.Size = new System.Drawing.Size(75, 23);
+			this.btnExit.TabIndex = 7;
+			this.btnExit.Text = "Exit";
+			this.btnExit.UseVisualStyleBackColor = true;
+			// 
+			// dgvManufacturer
+			// 
+			this.dgvManufacturer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dgvManufacturer.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Manufacturer_ID,
+            this.Manufacturer_Name});
+			this.dgvManufacturer.Location = new System.Drawing.Point(6, 19);
+			this.dgvManufacturer.Name = "dgvManufacturer";
+			this.dgvManufacturer.Size = new System.Drawing.Size(468, 128);
+			this.dgvManufacturer.TabIndex = 0;
+			// 
+			// Manufacturer_ID
+			// 
+			this.Manufacturer_ID.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+			this.Manufacturer_ID.FillWeight = 70F;
+			this.Manufacturer_ID.HeaderText = "Manufacturer ID";
+			this.Manufacturer_ID.Name = "Manufacturer_ID";
+			// 
+			// Manufacturer_Name
+			// 
+			this.Manufacturer_Name.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+			this.Manufacturer_Name.HeaderText = "Manufacturer Name";
+			this.Manufacturer_Name.Name = "Manufacturer_Name";
+			// 
+			// frmManufacturer
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(504, 316);
+			this.Controls.Add(this.btnExit);
+			this.Controls.Add(this.btnSkip);
+			this.Controls.Add(this.btnSave);
+			this.Controls.Add(this.btnEdit);
+			this.Controls.Add(this.btnDelete);
+			this.Controls.Add(this.btnAdd);
+			this.Controls.Add(this.groupBox2);
+			this.Controls.Add(this.groupBox1);
+			this.Name = "frmManufacturer";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Manufacturer";
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.groupBox2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.dgvManufacturer)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.TextBox txtManufacturerName;
+		private System.Windows.Forms.TextBox txtManufacturerID;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.DataGridView dgvManufacturer;
+		private System.Windows.Forms.Button btnAdd;
+		private System.Windows.Forms.Button btnDelete;
+		private System.Windows.Forms.Button btnEdit;
+		private System.Windows.Forms.Button btnSave;
+		private System.Windows.Forms.Button btnSkip;
+		private System.Windows.Forms.Button btnExit;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Manufacturer_ID;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Manufacturer_Name;
+	}
+}
